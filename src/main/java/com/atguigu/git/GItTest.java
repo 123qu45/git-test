0b7dea1d53a28389d9770bd19f2de86dbf1f423f
Layne <Layne@atguigu.com>
2022-08-01 22:44:32 +0800

    master commit2

--- src/main/java/com/atguigu/git/GItTest.java
+++ src/main/java/com/atguigu/git/GItTest.java
@@ -12,6 +12,7 @@ public class GItTest {
         System.out.println("hello git5!");
         System.out.println("hello git6!");
         System.out.println("master test!");
+        System.out.println("master test1!");
         System.out.println("hot-fix test!");
         System.out.println("push test!");
         System.out.println("pull test!");
